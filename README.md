3960cd23eb81f3ec09928af60db923f762c43b4f
fanyifanbumaimeng <1048429929@qq.com>
2018-01-21 23:03:04 +0800

    提供四舍五入，向下取整，向上取整不同的模式

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # vue-starLevelComponent
-一款vue 的评分插件。
+
+> 一款vue 的评分插件。
+
+
+## 运行DEMO项目
+
+```
+下载然后打开starRating页面
+```
+## 特点
+1. 简单易用，提供显示和评分两种模式
+2. 支持四舍五入，向上取整，向下取整，默认四种模式
+3. 提供单独以组件的方式按需引入
+
+## 获取
+
+
+## 用法
+
+```
+
+### 以组件的形式安装
+```
+// 在组件中导入插件
+import starRating from './starRating/starRating.vue'
+// 在组件中注册插件
+export default {
+  components: { starRating }
+}
+// 在组件的 temelate 中直接使用
+        <star-Rating
+                :score=3.2
+                :can-Choose=true
+                :mode="roundUp"
+                v-on:change="afterClick"
+        ></star-Rating>
+```
+
+## 选项
+你可以通过在所在的元素上设置以下属性来配置`starRating`
+1. `mode`：默认不进行四舍五入，显示半星
+    'round'代表四舍五入
+    'roundUp'代表向上取整
+    'roundDown'代表向下取整
+2. `canChoose'，是否是评分模式，默认为false显示模式，true为评分模式
+3. 'score'，单个评分，跟max总分配合使用，默认max为5.
+
+## 事件
+`change` 点击进行评分时进行触发
+
---
+++ statRating/starRating.vue
@@ -0,0 +1,128 @@
+<template lang="html">
+  <div class="star-component">
+            <span v-for="(item,index) in scoreLevel"
+                :key="index"
+                :class="[item=='on'?'star-item-on':item=='half'?'star-item-half':'star-item-off']"
+                @click="chooseStar(index)"
+                  class="star-item"
+            >
+            </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'starRating',
+  props: {
+      //单个评分
+      score:{
+          type: Number,
+          default:3
+      },
+      //是否是选择模式
+      canChoose:{
+          type:Boolean,
+          default:false
+      },
+      //总数
+      max:{
+          type:Number,
+          default:5
+      },
+      //四舍五入模式，默认不四舍五入。
+      //默认模式 normal
+      //四舍五入模式 round
+      //向上取整 roundUp
+      //向下取整 roundDown
+      mode:{
+          type:String,
+          default:"normal"
+      }
+  },
+  data () {
+      return {
+          //是否已经选择
+          hasChoosed:false,
+          //传入组件的单个评分的映射
+          selfScore:this.score
+      }
+  },
+  computed: {
+      scoreLevel:function(){
+          //处理最大值情况
+          var max = this.max;
+          var score =this.selfScore>max?max:this.selfScore;
+          var mode = this.mode;
+
+          //每颗星星对应的状态
+          var result = [];
+          score = (score*10/max)/2;
+          //向上取整
+          if(mode==="roundUp"){
+              score=Math.ceil(score)
+          }
+          //向下取整
+          if(mode==="roundDown"){
+              score=Math.floor(score)
+          }
+          //四舍五入
+          if(mode==="round"){
+              score = score.toFixed(0)
+          }
+
+          //取整
+          var integer = Math.floor(score);
+          //是否有小数
+          hasDecimal = score%1!==0;
+          //满星
+          for(var i=0;i<integer;i++){
+              result.push("on");
+          }
+          //半星
+          if(hasDecimal){
+              result.push("half")
+          }
+          //无星
+          while(result.length<5){
+              result.push("off")
+          }
+          return result;
+      }
+  },
+  methods: {
+      chooseStar:function(index,$event){
+          //判断是否是第一次选择
+          if(this.canChoose&&!this.hasChoosed){
+              this.selfScore=(this.max*(index+1)/5);
+              this.hasChoosed=true;
+              this.$emit('change', index);
+          }
+      }
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style lang="css" scoped>
+    .star-item{
+        display: inline-block;
+        background-repeat:no-repeat;
+        width:20px;
+        height:20px;
+        margin-right:22px;
+        -webkit-background-size: 100%;
+        background-size: 100%;
+
+    }
+    .star-item-on{
+        background: url("../images/star-off.png") 0 0 no-repeat;
+    }
+    .star-item-half{
+        background: url("../images/star-half.png")  0 0 no-repeat;
+    }
+    .star-item-off{
+        background: url("../images/star-on.png")  0 0 no-repeat;
+    }
+</style>
